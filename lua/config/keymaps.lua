f843050901a48a0bbd51f90e49e0237ee8bb41ca
Toby <bytao@icloud.com>
2025-11-18 17:12:27 +0800

    up keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,13 @@
 local map = vim.keymap.set
 
 -- 插入模式下的光标移动（类似 Emacs 风格）
-map("i", "<C-h>", "<Left>", { desc = "Move left" })
-map("i", "<C-l>", "<Right>", { desc = "Move right" })
+map("i", "<C-b>", "<Left>", { desc = "Move left" })
+map("i", "<C-f>", "<Right>", { desc = "Move right" })
 
 -- 移动到行首/行尾
 map("i", "<C-a>", "<Home>", { desc = "Move to line start" })
 map("i", "<C-e>", "<End>", { desc = "Move to line end" })
 
 -- 按单词移动
-map("i", "<C-b>", "<C-Left>", { desc = "Move word backward" })
-map("i", "<C-f>", "<C-Right>", { desc = "Move word forward" })
+map("i", "<C-n>", "<C-Down>", { desc = "Move cursor down one line" })
+map("i", "<C-p>", "<C-Up>", { desc = "Move cursor up one line" })
